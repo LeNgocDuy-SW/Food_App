6e1776fa32e206a40d1b59bbc93ca157d1f6e6ef
LeNgocDuy-SW <leduy2k5bavi@gmail.com>
2026-07-28 10:26:52 +0700

    Fix missing os import

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 
